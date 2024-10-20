906b1f35eee9892165c6018517b7ae0604c5f4b1
Fernanda Vieira <vieiras.ferndanda@gmail.com>
2024-10-20 18:06:15 -0400

    first commit

---
+++ js/site.js
@@ -0,0 +1,41 @@
+﻿function compararAntes(anoLancamentoJogo) {
+    let anoLancamentoReferencia = @MusicaReferencia.Last().AnoLancamento;
+
+    if (anoLancamentoReferencia < anoLancamentoJogo) {
+        adicionarNovaCarta();
+    } else {
+        tocarNovaMusica();
+    }
+}
+
+function compararDepois(anoLancamentoJogo) {
+    let anoLancamentoReferencia = @MusicaReferencia.Last().AnoLancamento;
+
+    if (anoLancamentoReferencia > anoLancamentoJogo) {
+        adicionarNovaCarta();
+    } else {
+        tocarNovaMusica();
+    }
+}
+
+function adicionarNovaCarta() {
+    DotNet.invokeMethodAsync('TriviaMusical', 'AdicionarNovaCarta')
+        .then(() => {
+            console.log('Nova música adicionada com sucesso!');
+            // Aqui você pode atualizar ou manipular a interface se necessário
+        })
+        .catch(error => {
+            console.error('Erro ao adicionar nova música:', error);
+        });
+}
+
+function tocarNovaMusica() {
+    if (MusicaReferencia.Any()) {
+        let novaIdMusica = @MusicaReferencia.Last().IdMusica;
+        document.getElementById('player-musica').src = "https://www.youtube.com/embed/" + novaIdMusica + "?autoplay=1";
+    }
+    else {
+        adicionarNovaCarta();
+    }
+    
+}
